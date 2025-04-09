97f6c01d1ff4b76d999e961837bf175b4045900c
Umer Munir <umermunir414@gmail.com>
2025-04-09 14:00:13 +0500

    NavBar Done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,19 +6,26 @@ import About from './pages/About'
 import Contact from './pages/Contact'
 import Product from './pages/Product'
 import Cart from './pages/Cart'
+import Orders from './pages/Orders'
+import PlaceOrder from './pages/PlaceOrder'
+import Login from './pages/Login'
+import NavBar from './components/NavBar'
 
 
 const App = () => {
   return (
-    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[10vw] '>
-
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw] '>
+      <NavBar/>
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/collection' element={<Collection/>} />
         <Route path='/about' element={<About/>} />
         <Route path='/contact' element={<Contact/>} />
         <Route path='/product/:productid' element={<Product/>} />
-        <Route path='/cart2' element={<Cart/>} />
+        <Route path='/cart' element={<Cart/>} />
+        <Route path='/orders' element={<Orders/>} />
+        <Route path='/placeOrders' element={<PlaceOrder/>} />
+        <Route path='/login' element={<Login/>} />
 
       </Routes>
       
--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
+import {assets} from './../assets/assets'
+import { NavLink } from 'react-router-dom'
 
 const NavBar = () => {
   return (
-    <div>
+    <div className='py-4  flex justify-between items-center'>
+      <img src={assets?.logo} className='w-36'/>
+      <ul className=' hidden sm:flex sm:gap-5 lg:gap-14 sm:font-bold sm:text-sm lg:text-lg text-gray-700'>
+        <NavLink to='/' className='flex flex-col items-center gap-0.5 hover:text-gray-500'>
+          <p>HOME</p>
+          <hr  className='hidden w-3/4 h-[1.5px] border-none bg-gray-700' />
+        </NavLink>
+        <NavLink to='/collection' className='flex flex-col items-center gap-0.5 hover:text-gray-500'>
+          <p>COLLECTION</p>
+          <hr className='hidden w-3/4 h-[1.5px] border-none bg-gray-700' />
+        </NavLink>
+        <NavLink to='/about' className='flex flex-col items-center gap-0.5 hover:text-gray-500'>
+          <p>ABOUT</p>
+          <hr className='hidden w-3/4 h-[1.5px] border-none bg-gray-700' />
+        </NavLink>
+        <NavLink to='/contact' className='flex flex-col items-center gap-0.5 hover:text-gray-500'>
+          <p>CONTACT</p>
+          <hr className='hidden w-3/4 h-[1.5px] border-none bg-gray-700 ' />
+        </NavLink>
+       
+      </ul>
+      <div className='flex gap-5  items-center'>
+        <img src={assets?.search_icon}  className='w-5 cursor-pointer' alt="" />
+        <div className='group relative'>
+          <div className='w-7 p-1 cursor-pointer rounded hover:bg-slate-200'>
+          <img src={assets?.profile_icon} className=' ' alt="" />
+          </div>
+          <div className='hidden group-hover:block  absolute dropdown-menu right-0  pt-2'>
+            <div className='flex flex-col gap-2 bg-slate-200 py-2 px-2 rounded w-50 text-slate-600  '>
+              <p className='hover:text-black cursor-pointer'>My Profile</p>
+              <p className='hover:text-black cursor-pointer'>Orders</p>
+              <p className='hover:text-black cursor-pointer'>Logout</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
       
     </div>
   )
