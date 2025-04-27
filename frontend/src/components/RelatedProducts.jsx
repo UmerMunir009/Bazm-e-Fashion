a5e0b9c3dc5821f69b42e9ff2ee532b08e477e34
TalhaYounas6 <myounas1168@gmail.com>
2025-04-27 12:42:02 +0500

    Frontend Completed

--- frontend/src/components/RelatedProducts.jsx
+++ frontend/src/components/RelatedProducts.jsx
@@ -2,29 +2,39 @@ import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 import ProductItem from './ProductItem';
 
-const RelatedProducts = ({category,subCategory}) => {
-    const {products} =useContext(ShopContext);
-    const [filteredProducts,setFilteredProducts]=useState([]);
+const RelatedProducts = ({category, subCategory}) => {
+    const {products} = useContext(ShopContext);
+    const [filteredProducts, setFilteredProducts] = useState([]);
 
-    useEffect(()=>{
-        if(products.length>0){
-            let copy=products.slice();
-            copy=copy.filter(item=>item.category===category && item.subCategory === subCategory)
-            setFilteredProducts(copy.slice(0,5))
+    useEffect(() => {
+        if(products.length > 0) {
+            let copy = products.slice();
+            copy = copy.filter(item => item.category === category && item.subCategory === subCategory)
+            setFilteredProducts(copy.slice(0, 5))
         }
+    }, [products]);
 
-    },[products])
-  return filteredProducts.length>0 ? (
-    
-    <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-5 mb-5'>
-    {filteredProducts.map((item,index)=>(
-        <ProductItem  key={index} id={item?._id} name={item?.name} image={item?.image} price={item?.price}/>
-    ))
 
-    }
-    </div>
-  ):     <div className='mt-5'><p className='text-lg text-gray-500'>No Items to show</p></div>
+    const handleItemClick = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    };
 
+    return filteredProducts.length > 0 ? (
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-5 mb-5">
+            {filteredProducts.map((item, index) => (
+                <div key={index} onClick={handleItemClick} className="scroll-smooth flex">
+                    <ProductItem  id={item?._id}  name={item?.name}  image={item?.image}  price={item?.price}/>
+                </div>
+            ))}
+        </div>
+    ) : (
+        <div className='mt-5'>
+            <p className='text-lg text-gray-500'>No Items to show</p>
+        </div>
+    );
 }
 
-export default RelatedProducts
+export default RelatedProducts;
--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import Title from './../components/Title'
 import { assets } from '../assets/assets'
 import NewsLetterBox from './../components/NewsLetterBox'
+import { useNavigate } from 'react-router-dom'
 
 const Contact = () => {
+  const navigate=useNavigate();
   return (
     <div>
       <div className='text-2xl border-t border-gray-300 pt-10 text-center'>
@@ -17,7 +19,7 @@ const Contact = () => {
           <p className='text-gray-600'>Tel : 0343-0468877 <br />umermunir1023@gmail.com</p>
           <p className='text-gray-600'>Learn more about our team and explore our <br />store now.</p>
           
-          <button onClick={()=>navigate('/placeOrders')} className='bg-white cursor-pointer text-black border border-gray-500 text-xs  py-3 px-2 mt-4 rounded hover:bg-black hover:text-white active:bg-white active:text-black min-w-40'>SHOP NOW</button>
+          <button onClick={()=>navigate('/collection')} className='bg-white cursor-pointer text-black border border-gray-500 text-xs  py-3 px-2 mt-4 rounded hover:bg-black hover:text-white active:bg-white active:text-black min-w-40'>SHOP NOW</button>
         
         </div>
 
