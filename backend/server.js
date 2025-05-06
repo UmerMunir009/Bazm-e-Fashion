97df54b4dc2927d156e29a25d57963274bd151f5
Umer Munir <umermunir414@gmail.com>
2025-05-07 00:35:33 +0500

    MongoDB and Cloudinary are connected to project

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import connectDB from './config/mongodb.js';
+import connectCloudinary from './config/cloudinary.js';
 
 //App config
 const app = express();
 const port = process.env.PORT || 4000;
+connectDB(); //mongodb connection established here with project
+connectCloudinary(); //cloudinary connection established here with project
+
 
 //Middleware
 app.use(express.json()); //any request that will come,will get parsed into json
@@ -14,3 +19,6 @@ app.use(cors()); //allow cross-origin requests or we can accss the backend from 
 app.get('/', (req, res) => {
     res.send('API is working well')
 })
+
+//start the server
+app.listen(port,()=>console.log(`Server is running on port ${port}`))
